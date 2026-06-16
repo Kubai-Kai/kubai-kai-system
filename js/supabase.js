4b838b6799d21383c2083dff2293344dc3f0ae4f
Kubai-Kai <Sebastian.weigert1988@gmx.de>
2026-06-16 23:20:15 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js'
 
 const supabaseUrl = 'https://swnsxlgtfqcwbkydsbhi.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3bnN4bGd0ZnFjd2JreWRzYmhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2MTE1NTcsImV4cCI6MjA5NzE4NzU1N30.6mQk2PESNf7iqmnFa3H1zgFZ85ji2Haeknm7RV2sAI4'
