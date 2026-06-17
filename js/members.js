d1ab4f30d787de28288ef8f5c13cc7250735d72a
Kubai-Kai <Sebastian.weigert1988@gmx.de>
2026-06-17 15:29:03 +0200

    Update members.js

--- js/members.js
+++ js/members.js
@@ -1,1 +1,15 @@
+import { supabase } from "./supabase.js";
 
+export async function getMembers() {
+  const { data, error } = await supabase
+    .from("members")
+    .select("*")
+    .order("created_at", { ascending: false });
+
+  if (error) {
+    console.error(error);
+    return [];
+  }
+
+  return data;
+}
